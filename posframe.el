91ec4135e0528e1a7ba2b445b74d1ea825ab6038
Feng Shu <tumashu@163.com>
2021-04-16 11:58:21 +0800

    v1.0.1

--- posframe.el
+++ posframe.el
@@ -5,7 +5,7 @@
 ;; Author: Feng Shu <tumashu@163.com>
 ;; Maintainer: Feng Shu <tumashu@163.com>
 ;; URL: https://github.com/tumashu/posframe
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Keywords: convenience, tooltip
 ;; Package-Requires: ((emacs "26"))
 
