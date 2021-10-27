0e87bc7a16d00cf0bdf1fb4dbe7393e4cf086581
Feng Shu <tumashu@163.com>
2021-10-27 11:37:38 +0800

    * posframe.el (posframe--mouse-banish): let x and y >=0.

--- posframe.el
+++ posframe.el
@@ -742,7 +742,7 @@ https://github.com/tumashu/posframe/issues/4#issuecomment-357514918"
                (< m-x (+ x w))
                (> m-y y)
                (< m-y (+ y h)))
-      (set-mouse-pixel-position parent-frame (- x 5) (- y 10)))))
+      (set-mouse-pixel-position parent-frame (max 0 (- x 5)) (max 0 (- y 10))))))
 
 (defun posframe--redirect-posframe-focus ()
   "Redirect focus from the posframe to the parent frame.
