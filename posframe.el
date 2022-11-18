e0a2964d86981bca7f2e67a814acd4b5d6ded5b6
Feng Shu <tumashu@163.com>
2022-11-18 12:48:51 +0800

    Improve docstring of posframe-poshandler-point-frame-center.

--- posframe.el
+++ posframe.el
@@ -1241,8 +1241,9 @@ The structure of INFO can be found in docstring of
 (defun posframe-poshandler-point-frame-center (info)
   "Posframe's position handler.
 
-This poshandler function let top edge center of posframe align to
-center of frame line at point.
+This poshandler function let center of posframe vertical align to
+center of frame and top edge of posframe horizontal align to
+buttom edge of current point.
 
 The structure of INFO can be found in docstring of
 `posframe-show'."
