3298e760b64fbe1449a266c8debe7ed2c2d4b907
Feng Shu <tumashu@163.com>
2024-04-06 07:40:16 +0800

    Fix: posframe is NOT part of GNU Emacs #137

--- posframe.el
+++ posframe.el
@@ -9,7 +9,7 @@
 ;; Keywords: convenience, tooltip
 ;; Package-Requires: ((emacs "26.1"))
 
-;; This file is part of GNU Emacs.
+;; This file is not part of GNU Emacs.
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
