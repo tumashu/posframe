tumashu/posframe

commit 6ac22a6419f964032b734133c6aa19c5c124720c

If frame position is already correct, don't call set-frame-position
That creates unnecessary work/refresh too.

posframe.el
@@ -961,7 +961,8 @@ This need PARENT-FRAME-WIDTH and PARENT-FRAME-HEIGHT"
                (equal posframe--last-posframe-displayed-size
                       (cons (frame-pixel-width posframe)
                             (frame-pixel-height posframe))))
-    (set-frame-position posframe (car position) (cdr position))
+    (unless (equal (frame-position posframe) position)
+      (set-frame-position posframe (car position) (cdr position)))
     (setq-local posframe--last-posframe-pixel-position position)
     (setq-local posframe--last-parent-frame-size
                 (cons parent-frame-width parent-frame-height))

commit 1f9d544697cbf25071da62c81cc3972770bb9ebf

Re-fix #65
It regressed in my change 6ac22a6419f964032b734133c6aa19c5c124720c.

The position can use negative values, and then the coordinate of the
top-left corner of the posframe depends on its dimensions.

posframe.el
@@ -988,11 +988,12 @@ This need PARENT-FRAME-WIDTH and PARENT-FRAME-HEIGHT"
                ;; the posframe.
                (equal posframe--last-parent-frame-size
                       (cons parent-frame-width parent-frame-height))
-               (equal posframe--last-posframe-displayed-size
-                      (cons (frame-pixel-width posframe)
-                            (frame-pixel-height posframe))))
-    (unless (equal (frame-position posframe) position)
-      (set-frame-position posframe (car position) (cdr position)))
+               (or (and (>= (car position) 0)
+                        (>= (cdr position) 0))
+                   (equal posframe--last-posframe-displayed-size
+                          (cons (frame-pixel-width posframe)
+                                (frame-pixel-height posframe)))))
+    (set-frame-position posframe (car position) (cdr position))
     (posframe--save-new-posframe-position posframe position
                                           parent-frame-width
                                           parent-frame-height))